6f4ac9f90df5a6b412be5d4c8837aee046163e51
Andriamahay11master <hirimanana@yahoo.fr>
2024-03-11 16:58:14 +0300

    MAJ footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,34 +3,15 @@ import './footer.scss';
 import Link from 'next/link';
 
 interface FooterProps{
-    desc: string,
-    title: string,
+    desc?: string,
+    title?: string,
     copyright: string
 }
 
-export default function Footer({desc, title, copyright} : FooterProps) {
+export default function Footer({copyright} : FooterProps) {
     
     return (
         <footer className='footerPage'>
-            <div className="cntFooter">
-                <div className="colFooter">
-                    <h2 className='title-h2 titleFooter'>MindMoney </h2>
-                    <p className="txtFooter">
-                        {desc}
-                    </p>
-                </div>
-                <div className="colFooter">
-                    <div className="titleFooter">{title}</div>
-                    <div className="listLink">
-                        <div className="itemLink">
-                            <Link href="https://www.facebook.fr" target='_blank' aria-label="Profil Facebook d'Andriamahay Henikaja Irimanana" title='Profil Facebook'><i className="icon-footer icon-fb"></i></Link>
-                        </div>
-                        <div className="itemLink">
-                            <Link href="https://www.linkedin.com/in/andriamahay-henikaja-irimanana/" target='_blank' aria-label="Profil LinkedIn d'Andriamahay Henikaja Irimanana" title='Profil LinkedIn'><i className="icon-footer icon-linkedin"></i></Link>
-                        </div>
-                    </div>
-                </div>
-            </div> 
             <div className="footerCopyright">
                 <div className="cntCopyright">
                     <div className="itemCol"> 
