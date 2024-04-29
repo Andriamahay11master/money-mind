2837d4402b76fcee75324e7af4aa85eb881660f2
Andriamahay11master <hirimanana@yahoo.fr>
2024-04-29 18:00:59 +0300

    add manifest json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <link rel="manifest" href="manifest.json" />
       <link rel="icon" href="favicon.ico" sizes="any" />
       <link
           rel="icon"
